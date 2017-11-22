5b70f5b4d6fcce1b5e7eb2def750e491e26af32c
Alberto Rocha <betogrun@gmail.com>
2017-11-22 11:35:43 -0200

    Update template and migrations

--- db/migrate/20150901171448_create_spree_tenants.rb
+++ db/migrate/20150901171448_create_spree_tenants.rb
@@ -1,4 +1,4 @@
-class CreateSpreeTenants < ActiveRecord::Migration
+class CreateSpreeTenants < ActiveRecord::Migration[5.1]
   def change
     create_table :spree_tenants do |t|
       t.string :name
